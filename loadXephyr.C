1ce21557b84618b451ad666ba98dcb6bf18587d7
Ale <pan.manfredini@gmail.com>
2018-04-28 17:21:04 +0300

    fuck you

--- loadXephyr.C
+++ loadXephyr.C
@@ -10,5 +10,7 @@ int loadXephyr(){
   gROOT->ProcessLine(".L ../Xephyr/src/ToyGenerator.cxx+g");
   gROOT->ProcessLine(".L ../Xephyr/src/plotHelpers.cxx+g");
   gROOT->ProcessLine(".L ../Xephyr/src/ToyFitterExclusion.cxx+g");
+    gInterpreter->AddIncludePath("../Xephyr/src"); // in this case is just XEPHYR src from next dir.
+    gROOT->ProcessLine(".L ../SR1/StatisticalAnalyses/xephyr_sr1_likelihood/src/likelihoodDef.cxx");
   return 0;
 }
